a3a5b7b9b0f4438ddcfab3af0d216e5c12464287
prayag0one4 <142918263+prayag0one4@users.noreply.github.com>
2024-11-20 00:59:35 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#90EE90",
+      },
+
+    },
+  },
+
+  plugins: [],
+}
+
